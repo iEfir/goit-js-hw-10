c7cdbf7e6fe259c5e2c51dc7ab22d444f6ca5561
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-02-12 17:07:46 +0200

    task-10 done

---
+++ src/countries-service.js
@@ -0,0 +1,13 @@
+// const BASE_URL = 'https://restcountries.com/v2';
+// const OPTIONS = 'fields=name,capital,population,flags,languages';
+
+export function fetchCountries(searchQuery) {
+  const BASE_URL = 'https://restcountries.com/v2';
+  const OPTIONS = 'fields=name,capital,population,flags,languages';
+  return fetch(`${BASE_URL}/name/${searchQuery}?${OPTIONS}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './countries-service';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  e.preventDefault();
+
+  const searchQueryValue = e.target.value.trim();
+  if (!searchQueryValue) {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    return;
+  }
+  fetchCountries(searchQueryValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if ((countries.length <= 10) & (countries.length >= 2)) {
+        const listOFCountries = countries.map(c => countryListEl(c));
+        countryList.innerHTML = listOFCountries.join('');
+        countryInfo.innerHTML = '';
+      }
+      if (countries.length === 1) {
+        const dataOFCountry = countries.map(c => countryInfoEl(c));
+        countryInfo.innerHTML = dataOFCountry.join('');
+        countryList.innerHTML = '';
+      }
+    })
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
+}
+
+function countryListEl({ name, flags }) {
+  return `<li class='country-list__item'><img width='50' height='50' src='${flags.svg}'/><h2 class='lis-item-title'>${name}</h2></li>`;
+}
+
+function countryInfoEl({ name, capital, flags, population, languages }) {
+  return `<div class='country-info__container'><img width='50' height='50' src='${flags.svg}'/><h2 class='item-title'>${name}</h2></div>
+  <ul><li><b>Capital:</b><p>${capital}</p></li><li><b>Population:</b><p>${population}</p></li><li><b>Languages:</b><p>${languages[0].name}</p></li></ul>`;
+}
